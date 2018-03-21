b7760d0ad8a534ad5d7f437dfac2364f32e57289
tilsche <thomas.ilsche@tu-dresden.de>
2018-03-21 11:01:26 +0100

    prepare connection for topic rpc

--- include/dataheap2/connection.hpp
+++ include/dataheap2/connection.hpp
@@ -22,23 +22,26 @@ public:
     void main_loop();
 
 protected:
-    using ManagementCallback = std::function<void(const json& response)>;
+    using ManagementCallback = std::function<json(const json& response)>;
+    using ManagementResponseCallback = std::function<void(const json& response)>;
 
     explicit Connection(const std::string& connection_token, std::size_t concurrency_hint = 1);
     virtual ~Connection() = 0;
 
     void connect(const std::string& server_address);
+    virtual void setup_complete() = 0;
 
-    virtual void config_callback(const json& config) = 0;
-
-    void send_management(const std::string& function, json payload = json());
-    void register_management_callback(const std::string& function, ManagementCallback cb);
+    void rpc(const std::string& function, ManagementResponseCallback callback,
+             json payload = json());
+    void register_management_callback(const std::string& function, ManagementCallback callback);
 
     void stop();
     virtual void close();
 
 private:
-    void dispatch_management(const AMQP::Message& message);
+    void handle_management_message(const AMQP::Message& incoming_message, uint64_t deliveryTag,
+                                   bool redelivere);
+    void handle_broadcast_message(const AMQP::Message& message);
 
 protected:
     asio::io_service io_service;
@@ -51,6 +54,9 @@ private:
     std::unique_ptr<AMQP::TcpConnection> management_connection_;
     std::unique_ptr<AMQP::TcpChannel> management_channel_;
     std::unordered_map<std::string, ManagementCallback> management_callbacks_;
+    std::unordered_map<std::string, ManagementResponseCallback> management_rpc_response_callbacks_;
     std::string management_queue_;
+    std::string management_exchange_ = "dh2.management";
+    std::string management_broadcast_exchange_ = "dh2.broadcast";
 };
 } // namespace dataheap2
--- src/connection.cpp
+++ src/connection.cpp
@@ -1,5 +1,7 @@
 #include <dataheap2/connection.hpp>
 
+#include "util.hpp"
+
 #include <amqpcpp.h>
 
 #include <nlohmann/json.hpp>
@@ -10,12 +12,11 @@
 
 namespace dataheap2
 {
-const std::string management_queue = "managementQueue";
+const std::string management_queue = "dh2.management";
 
 Connection::Connection(const std::string& connection_token, std::size_t concurrency_hint)
 : io_service(concurrency_hint), handler(io_service), connection_token_(connection_token)
 {
-    register_management_callback("config", [this](auto& response) { config_callback(response); });
 }
 
 Connection::~Connection()
@@ -27,49 +28,41 @@ void Connection::main_loop()
     io_service.run();
 }
 
+static auto debug_error_cb(const std::string& prefix)
+{
+    return [prefix](const auto& message) { std::cerr << prefix << ": " << message << std::endl; };
+}
+
+static auto debug_success_cb(const std::string& prefix)
+{
+    return [prefix]() { std::cerr << prefix << std::endl; };
+}
+
 void Connection::connect(const std::string& server_address)
 {
     std::cerr << "connecting to management server: " << server_address << std::endl;
     management_connection_ =
         std::make_unique<AMQP::TcpConnection>(&handler, AMQP::Address(server_address));
     management_channel_ = std::make_unique<AMQP::TcpChannel>(management_connection_.get());
-    management_channel_->onError([](const char* message) {
-        std::cerr << "management channel error: " << message << std::endl;
-        throw std::runtime_error(message);
-    });
+    management_channel_->onError(debug_error_cb("management channel error"));
 
-    management_channel_
-        ->declareQueue(AMQP::exclusive) //  rpc queue
+    management_queue_ = std::string("client-") + connection_token_ + "-rpc";
+
+    management_channel_->declareQueue(management_queue, AMQP::exclusive)
         .onSuccess([this](const std::string& name, int msgcount, int consumercount) {
             management_queue_ = name;
-
-            auto start_cb = [](const std::string& consumertag) {
-                std::cerr << "management consume operation started: " << consumertag << std::endl;
-            };
-
-            auto error_cb = [](const char* message) {
-                std::cerr << "management consume operation failed: " << message << std::endl;
-            };
-
-            // callback operation when a message was received
-            auto message_cb = [this](const AMQP::Message& message, uint64_t deliveryTag,
-                                     bool redelivered) {
-                std::cerr << "message received: " << std::endl;
-
-                dispatch_management(message);
-                management_channel_->ack(deliveryTag);
-            };
-
-            management_channel_->consume(name)
-                .onReceived(message_cb)
-                .onSuccess(start_cb)
-                .onError(error_cb);
-
-            // request initial config
-            send_management("register");
-            // TODO call subclasses on_init or so
+            management_channel_->bindQueue(management_broadcast_exchange_, management_queue_, "#")
+                .onError(debug_error_cb("error binding management queue to broadcast exchange"))
+                .onSuccess([this, name]() {
+                    management_channel_->consume(name)
+                        .onReceived([this](const AMQP::Message& message, uint64_t delivery_tag,
+                                           bool redelivered) {
+                            handle_management_message(message, delivery_tag, redelivered);
+                        })
+                        .onSuccess([this]() { setup_complete(); })
+                        .onError(debug_error_cb("management consume error"));
+                });
         });
-    ;
 }
 
 void Connection::register_management_callback(const std::string& function, ManagementCallback cb)
@@ -78,27 +71,57 @@ void Connection::register_management_callback(const std::string& function, Manag
     assert(ret.second);
 }
 
-void Connection::send_management(const std::string& function, json payload)
+void Connection::rpc(const std::string& function, ManagementResponseCallback response_callback,
+                     json payload)
 {
-    assert(payload.count("function") == 0);
-    payload["function"] = function;
     std::string message = payload.dump();
     AMQP::Envelope env(message.data(), message.size());
 
+    auto correlation_id = uuid(std::string("dh2-rpc-") + connection_token_ + "-");
+    env.setCorrelationID(correlation_id);
     env.setAppID(connection_token_);
     assert(!management_queue_.empty());
     env.setReplyTo(management_queue_);
 
-    // publish to default direct exchange
-    management_channel_->publish("", management_queue, env);
+    auto ret =
+        management_rpc_response_callbacks_.emplace(correlation_id, std::move(response_callback));
+    assert(ret.second);
+    management_channel_->publish(management_broadcast_exchange_, function, env);
 }
 
-void Connection::dispatch_management(const AMQP::Message& message)
+void Connection::handle_management_message(const AMQP::Message& incoming_message,
+                                           uint64_t deliveryTag, bool redelivered)
 {
-    const std::string responseMessage(message.body(), static_cast<size_t>(message.bodySize()));
+    std::cerr << "management message received" << std::endl;
+
+    const std::string content_str(incoming_message.body(),
+                                  static_cast<size_t>(incoming_message.bodySize()));
     try
     {
-        management_callbacks_.at(message.correlationID())(json::parse(responseMessage));
+        auto content = json::parse(content_str);
+
+        if (auto it = management_rpc_response_callbacks_.find(incoming_message.correlationID());
+            it != management_rpc_response_callbacks_.end())
+        {
+            // Incoming message is a RPC-response, call the response handler
+            it->second(content);
+        }
+        else if (auto it = management_callbacks_.find(incoming_message.routingkey());
+                 it != management_callbacks_.end())
+        {
+            // incoming message is a RPC-call
+            auto response = it->second(content);
+
+            std::string reply_message; // = response.dump();
+            AMQP::Envelope env(reply_message.data(), reply_message.size());
+            env.setCorrelationID(incoming_message.correlationID());
+            env.setAppID(connection_token_);
+            management_channel_->publish("", incoming_message.replyTo(), env);
+        }
+        else
+        {
+            std::cerr << "message not found as rpc response or callback." << std::endl;
+        };
     }
     catch (nlohmann::json::parse_error& e)
     {
@@ -108,11 +131,8 @@ void Connection::dispatch_management(const AMQP::Message& message)
     {
         std::cerr << "error in rpc response: accessing parameter: " << e.what() << std::endl;
     }
-    catch (std::out_of_range& e)
-    {
-        std::cerr << "RPC response to unknown correlation id: " << message.correlationID()
-                  << std::endl;
-    }
+
+    management_channel_->ack(deliveryTag);
 }
 
 void Connection::close()
@@ -131,6 +151,6 @@ void Connection::stop()
     std::cerr << "requesting stop." << std::endl;
     close();
     std::cerr << "stopping io_service." << std::endl;
-    // Note the io_service will stop itself once all connections are closed
+    // the io_service will stop itself once all connections are closed
 }
 } // namespace dataheap2
