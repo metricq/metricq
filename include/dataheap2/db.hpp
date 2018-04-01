f75f91e4266ce36d861f7bade3854d0efc8b2291
Franz Schmidt <kinnarr@hollywuuud.de>
2018-04-01 21:43:35 +0200

    Updates db for new sink api

--- include/dataheap2/db.hpp
+++ include/dataheap2/db.hpp
@@ -8,5 +8,10 @@ class Db : public Sink
 {
 public:
     Db(const std::string& token);
+
+protected:
+    virtual void db_config_callback(const json& config);
+    void config_callback(const json& response);
+    void setup_complete() override;
 };
 } // namespace dataheap2
