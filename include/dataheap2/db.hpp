6a0a93618e14ea1140bbde1081703467dcb6cc76
Franz Schmidt <kinnarr@hollywuuud.de>
2018-04-02 19:22:01 +0200

    Addes history queue api to db

--- include/dataheap2/db.hpp
+++ include/dataheap2/db.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <dataheap2/history.pb.h>
 #include <dataheap2/sink.hpp>
 
 #include <nlohmann/json_fwd.hpp>
@@ -14,25 +15,18 @@ public:
     Db(const std::string& token);
 
 protected:
-    virtual json history_callback(const std::string& id, const json& content);
-    virtual std::vector<TimeValue> history_callback(const std::string& id,
-                                                    const std::string& from_timestamp,
-                                                    const std::string& to_timestamp,
-                                                    int interval_ms, int max_datapoints);
+    virtual HistoryResponse history_callback(const std::string& id, const HistoryRequest& content);
 
 protected:
     void history_callback(const AMQP::Message&);
     void setup_history_queue(const AMQP::QueueCallback& callback);
-
-protected:
     virtual void db_config_callback(const json& config);
     void config_callback(const json& response);
     void setup_complete() override;
-    json get_metrics_callback(const json& response);
 
 protected:
     std::string history_queue_;
     // Stored permanently to avoid expensive allocations
-    DataChunk data_chunk_;
+    HistoryRequest history_request_;
 };
 } // namespace dataheap2
--- src/db.cpp
+++ src/db.cpp
@@ -9,11 +9,6 @@ Db::Db(const std::string& token) : Sink(token)
 {
 }
 
-json Db::get_metrics_callback(const json& response)
-{
-    return {};
-}
-
 void Db::setup_history_queue(const AMQP::QueueCallback& callback)
 {
     assert(!data_server_address_.empty());
@@ -37,11 +32,12 @@ void Db::history_callback(const AMQP::Message& incoming_message)
     const auto& metric_name = incoming_message.routingkey();
     auto message_string = std::string(incoming_message.body(), incoming_message.bodySize());
 
-    auto content = json::parse(message_string);
+    history_request_.Clear();
+    history_request_.ParseFromString(message_string);
 
-    auto response = history_callback(metric_name, content);
+    auto response = history_callback(metric_name, history_request_);
 
-    std::string reply_message = response.dump();
+    std::string reply_message = response.SerializeAsString();
     AMQP::Envelope envelope(reply_message.data(), reply_message.size());
     envelope.setCorrelationID(incoming_message.correlationID());
     envelope.setContentType("application/json");
@@ -49,29 +45,12 @@ void Db::history_callback(const AMQP::Message& incoming_message)
     data_channel_->publish("", incoming_message.replyTo(), envelope);
 }
 
-json Db::history_callback(const std::string& id, const json& content)
+HistoryResponse Db::history_callback(const std::string& id, const HistoryRequest& content)
 {
-    json response;
-
-    response["target"] = id;
-
-    response["datapoints"] = json::array();
-
-    for (TimeValue tv : history_callback(id, content["range"]["from"], content["range"]["to"],
-                                         content["intervalMs"], content["maxDataPoints"]))
-    {
-        response["datapoints"].push_back({ tv.value, tv.time.time_since_epoch().count() });
-    }
+    HistoryResponse response;
+    response.set_metric(id);
 
     return response;
-}
-
-std::vector<TimeValue> Db::history_callback(const std::string& id,
-                                            const std::string& from_timestamp,
-                                            const std::string& to_timestamp, int interval_ms,
-                                            int max_datapoints)
-{
-    return std::vector<TimeValue>();
 }
 
 void Db::db_config_callback(const json& config)
@@ -87,26 +66,48 @@ void Db::config_callback(const json& response)
 {
     data_server_address_ = response["dataServerAddress"];
     data_queue_ = response["dataQueue"];
+    history_queue_ = response["historyQueue"];
 
     db_config_callback(response["config"]);
 
     setup_data_queue([this](const std::string& name, int msgcount, int consumercount) {
+        log::notice("setting up data queue, msgcount: {}, consumercount: {}", msgcount,
                     consumercount);
-                    // we do not tolerate other consumers
-                    assert(consumercount == 0);
+        // we do not tolerate other consumers
+        assert(consumercount == 0);
 
-                    auto message_cb = [this](const AMQP::Message& message, uint64_t deliveryTag,
-                                             bool redelivered) {
-                        (void)redelivered;
+        auto message_cb = [this](const AMQP::Message& message, uint64_t deliveryTag,
+                                 bool redelivered) {
+            (void)redelivered;
 
-                        data_callback(message);
-                        data_channel_->ack(deliveryTag);
-                    };
+            data_callback(message);
+            data_channel_->ack(deliveryTag);
+        };
 
-                    data_channel_->consume(name)
-                        .onReceived(message_cb)
-                        .onSuccess(debug_success_cb("sink data channel consume success"))
-                        .onError(debug_error_cb("sink data channel consume error"));
+        data_channel_->consume(name)
+            .onReceived(message_cb)
+            .onSuccess(debug_success_cb("sink data channel consume success"))
+            .onError(debug_error_cb("sink data channel consume error"));
+    });
+
+    setup_history_queue([this](const std::string& name, int msgcount, int consumercount) {
+        log::notice("setting up history queue, msgcount: {}, consumercount: {}", msgcount,
+                    consumercount);
+        // we do not tolerate other consumers
+        assert(consumercount == 0);
+
+        auto message_cb = [this](const AMQP::Message& message, uint64_t deliveryTag,
+                                 bool redelivered) {
+            (void)redelivered;
+
+            history_callback(message);
+            data_channel_->ack(deliveryTag);
+        };
+
+        data_channel_->consume(name)
+            .onReceived(message_cb)
+            .onSuccess(debug_success_cb("sink data channel consume success"))
+            .onError(debug_error_cb("sink data channel consume error"));
     });
 }
 } // namespace dataheap2
