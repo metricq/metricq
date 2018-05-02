c06112d11d65119325a5b68007579bc8214b16bf
tilsche <thomas.ilsche@tu-dresden.de>
2018-05-02 14:58:06 +0200

    Move operator<< for Duration to std namespace so it can be used outside of dataheap2 namespace.

--- include/dataheap2/ostream.hpp
+++ include/dataheap2/ostream.hpp
@@ -4,14 +4,22 @@
 
 #include <ostream>
 
-namespace dataheap2
+// USE THIS HEADER WITH CARE
+// Don't use if you have other operator<< on std::chrono::duration defined
+
+// Unfortunately duration is actually a (using) std type, so we can't
+// define this in datheap2
+namespace std
 {
-inline std::ostream& operator<<(std::ostream& os, Duration duration)
+inline std::ostream& operator<<(std::ostream& os, dataheap2::Duration duration)
 {
     os << duration.count();
     return os;
 }
+} // namespace std
 
+namespace dataheap2
+{
 inline std::ostream& operator<<(std::ostream& os, TimePoint tp)
 {
     os << tp.time_since_epoch();
@@ -23,4 +31,4 @@ inline std::ostream& operator<<(std::ostream& os, TimeValue tv)
     os << tv.time << " " << tv.value;
     return os;
 }
-}
+} // namespace dataheap2
