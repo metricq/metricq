f7fb14c686391cd98c8bb30167ee8813712ea149
tilsche <thomas.ilsche@tu-dresden.de>
2018-09-25 21:37:27 +0200

    use GNUInstallDirs for consistency
    stuff now ends up in lib64, which we probably maybe want... do we?

--- cmake/metricqConfig.cmake
+++ cmake/metricqConfig.cmake
@@ -1,7 +1,8 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(fmt)
 find_dependency(amqpcpp)
 find_dependency(asio)
+find_dependency(fmt)
+find_dependency(json)
 
 include("${CMAKE_CURRENT_LIST_DIR}/metricqTargets.cmake")
