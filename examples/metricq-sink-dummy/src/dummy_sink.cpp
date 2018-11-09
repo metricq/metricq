e1a661fc3dae69f44683b2cf29d7e783424faff7
Mario Bielert <mario.bielert@tu-dresden.de>
2018-11-09 13:19:33 +0100

    Adds metriq-sink-dummy

---
+++ examples/metricq-sink-dummy/src/dummy_sink.cpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2018, ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of metricq nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#include "dummy_sink.hpp"
+#include "log.hpp"
+
+#include <metricq/ostream.hpp>
+#include <metricq/types.hpp>
+
+#include <cmath>
+
+DummySink::DummySink(const std::string& manager_host, const std::string& token)
+: metricq::Sink(token), signals_(io_service, SIGINT, SIGTERM)
+{
+    // Register signal handlers so that the daemon may be shut down.
+    signals_.async_wait([this](auto, auto signal) {
+        if (!signal)
+        {
+            return;
+        }
+        Log::info() << "Caught signal " << signal << ". Shutdown.";
+        stop_requested_ = true;
+    });
+
+    connect(manager_host);
+}
+
+void DummySink::setup_complete()
+{
+    start_time_ = metricq::Clock::now();
+}
+
+void DummySink::data_callback(const std::string& id, metricq::TimeValue tv)
+{
+    Log::info() << id << ": " << tv.value << "@" << tv.time;
+
+    message_count_++;
+
+    auto now = metricq::Clock::now();
+
+    auto time_since_last = now - step_time_;
+
+    if (time_since_last > std::chrono::seconds(10))
+    {
+        auto message_rate =
+            message_count_ /
+            (double)std::chrono::duration_cast<std::chrono::seconds>(now - start_time_).count();
+
+        auto step_message_rate =
+            (message_count_ - message_count_last_step_) /
+            (double)std::chrono::duration_cast<std::chrono::seconds>(now - step_time_).count();
+
+        Log::info() << "Overall message rate is " << std::fixed << std::setprecision(2)
+                    << message_rate << " msg/s! Current step: " << step_message_rate << " msg/s";
+
+        message_count_last_step_ = message_count_;
+        step_time_ = metricq::Clock::now();
+    }
+}
---
+++ examples/metricq-sink-dummy/src/dummy_sink.hpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2018, ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of metricq nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <metricq/chrono.hpp>
+#include <metricq/sink.hpp>
+
+#include <asio/signal_set.hpp>
+
+#include <atomic>
+
+class DummySink : public metricq::Sink
+{
+public:
+    DummySink(const std::string& manager_host, const std::string& token);
+
+private:
+    void setup_complete() override;
+    void data_callback(const std::string& id, metricq::TimeValue tv) override;
+
+    asio::signal_set signals_;
+
+    std::size_t message_count_ = 0;
+    std::size_t message_count_last_step_ = 0;
+    metricq::TimePoint start_time_;
+    metricq::TimePoint step_time_;
+
+    std::atomic<bool> stop_requested_ = false;
+};
