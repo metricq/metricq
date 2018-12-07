de058906cb760b8daf0142eb0ccd8787723f6d37
tilsche <thomas.ilsche@tu-dresden.de>
2018-12-07 16:11:26 +0100

    fix logger [

--- examples/metricq-logger-nitro/include/metricq/logger/nitro.hpp
+++ examples/metricq-logger-nitro/include/metricq/logger/nitro.hpp
@@ -53,7 +53,7 @@ namespace detail
 
             if (!r.tag().empty())
             {
-                s << r.tag() << "[";
+                s << r.tag() << "][";
             }
 
             s << r.severity() << "]: " << r.message() << '\n';
