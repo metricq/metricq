1f7e16278e667e1481a40ece6b86a648f759cd70
Mario Bielert <mario.bielert@tu-dresden.de>
2019-02-26 16:08:15 +0100

    Adds new source for stressing metricq

---
+++ examples/metricq-source-stress-test/src/main.cpp
@@ -0,0 +1,98 @@
+// Copyright (c) 2018, ZIH, Technische Universitaet Dresden, Federal Republic of Germany
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of metricq nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#include "stress_test_source.hpp"
+
+#include <metricq/logger/nitro.hpp>
+
+#include <nitro/broken_options/parser.hpp>
+
+#include <iostream>
+#include <string>
+
+using Log = metricq::logger::nitro::Log;
+
+int main(int argc, char* argv[])
+{
+    metricq::logger::nitro::set_severity(nitro::log::severity_level::info);
+
+    nitro::broken_options::parser parser;
+    parser.option("server", "The metricq management server to connect to.")
+        .default_value("amqp://localhost")
+        .short_name("s");
+    parser.option("token", "The token used for source authentication against the metricq manager.")
+        .default_value("source-stress-test");
+    parser.toggle("verbose").short_name("v");
+    parser.toggle("trace").short_name("t");
+    parser.toggle("quiet").short_name("q");
+    parser.toggle("help").short_name("h");
+    parser.option("interval", "Interval to generate data in milliseconds.")
+        .short_name("i")
+        .default_value("100");
+
+    try
+    {
+        auto options = parser.parse(argc, argv);
+
+        if (options.given("help"))
+        {
+            parser.usage();
+            return 0;
+        }
+
+        if (options.given("trace"))
+        {
+            metricq::logger::nitro::set_severity(nitro::log::severity_level::trace);
+        }
+        else if (options.given("verbose"))
+        {
+            metricq::logger::nitro::set_severity(nitro::log::severity_level::debug);
+        }
+        else if (options.given("quiet"))
+        {
+            metricq::logger::nitro::set_severity(nitro::log::severity_level::warn);
+        }
+
+        metricq::logger::nitro::initialize();
+
+        StressTestSource source(options.get("server"), options.get("token"),
+                                options.as<int>("interval"));
+        Log::info() << "starting main loop.";
+        source.main_loop();
+        Log::info() << "exiting main loop.";
+    }
+    catch (nitro::broken_options::parsing_error& e)
+    {
+        Log::warn() << e.what();
+        parser.usage();
+        return 1;
+    }
+    catch (std::exception& e)
+    {
+        Log::error() << "Unhandled exception: " << e.what();
+    }
+}
