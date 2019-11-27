c85cd5ec51b016a06aed8db8ca69b29804ac12f3
Franz Hoepfner <franz.schmidt2@tu-dresden.de>
2019-11-27 17:17:14 +0100

    Move get_metrics logic from metricq-manager to ManagementAgent baseclass

--- python/metricq/management_agent.py
+++ python/metricq/management_agent.py
@@ -1,5 +1,7 @@
+from itertools import islice
+
 import aio_pika
-from aiocouch import CouchDB
+from aiocouch import CouchDB, database
 
 from .agent import Agent
 from .logging import get_logger
@@ -11,18 +13,24 @@ logger = get_logger(__name__)
 
 class ManagementAgent(Agent):
     def __init__(
-        self, token, management_url, couchdb_url, couchdb_user, couchdb_password
+        self,
+        token,
+        management_url,
+        couchdb_url,
+        couchdb_user,
+        couchdb_password,
+        event_loop=None,
     ):
-        super().__init__(token, management_url)
+        super().__init__(token, management_url, event_loop=event_loop)
 
-        self.couchdb_client = CouchDB(
+        self.couchdb_client: CouchDB = CouchDB(
             couchdb_url,
             user=couchdb_user,
             password=couchdb_password,
             loop=self.event_loop,
         )
-        self.couchdb_db_config = None
-        self.couchdb_db_metadata = None
+        self.couchdb_db_config: database.Database = None
+        self.couchdb_db_metadata: database.Database = None
 
     async def connect(self):
         # First, connect to couchdb
@@ -47,3 +55,100 @@ class ManagementAgent(Agent):
             type=aio_pika.ExchangeType.FANOUT,
             durable=True,
         )
+
+    async def get_metrics(
+        self,
+        format="array",
+        historic=None,
+        selector=None,
+        prefix=None,
+        infix=None,
+        limit=None,
+    ):
+        if format not in ("array", "object"):
+            raise AttributeError("unknown format requested: {}".format(format))
+
+        if infix is not None and prefix is not None:
+            raise AttributeError('cannot get_metrics with both "prefix" and "infix"')
+
+        selector_dict = dict()
+        if selector is not None:
+            if isinstance(selector, str):
+                selector_dict["_id"] = {"$regex": selector}
+            elif isinstance(selector, list):
+                if len(selector) < 1:
+                    raise ValueError("Empty selector list")
+                if len(selector) == 1:
+                    # That may possibly be faster.
+                    selector_dict["_id"] = selector[0]
+                else:
+                    selector_dict["_id"] = {"$in": selector}
+            else:
+                raise TypeError(
+                    "Invalid selector type: {}, supported: str, list", type(selector)
+                )
+        if historic is not None:
+            if not isinstance(historic, bool):
+                raise AttributeError(
+                    'invalid type for "historic" argument: should be bool, is {}'.format(
+                        type(historic)
+                    )
+                )
+
+        # TODO can this be unified without compromising performance?
+        # Does this even perform well?
+        # ALSO: Async :-[
+        if selector_dict:
+            if historic is not None:
+                selector_dict["historic"] = historic
+            if prefix is not None or infix is not None:
+                raise AttributeError(
+                    'cannot get_metrics with both "selector" and "prefix" or "infix".'
+                )
+            aiter = self.couchdb_db_metadata.find(selector_dict, limit=limit)
+            if format == "array":
+                metrics = [doc["_id"] async for doc in aiter]
+            elif format == "object":
+                metrics = {doc["_id"]: doc.data async for doc in aiter}
+
+        else:  # No selector dict, all *fix / historic filtering
+            request_limit = limit
+            if infix is None:
+                request_prefix = prefix
+                if historic is not None:
+                    endpoint = self.couchdb_db_metadata.view("index", "historic")
+                else:
+                    endpoint = self.couchdb_db_metadata.all_docs
+            else:
+                request_prefix = infix
+                # These views produce stupid duplicates thus we must filter ourselves and request more
+                # to get enough results. We assume for no more than 6 infix segments on average
+                if limit is not None:
+                    request_limit = 6 * limit
+                if historic is not None:
+                    endpoint = self.couchdb_db_metadata.view("components", "historic")
+                else:
+                    raise NotImplementedError(
+                        "non-historic infix lookup not yet supported"
+                    )
+            if format == "array":
+                metrics = [
+                    key
+                    async for key in endpoint.ids(
+                        prefix=request_prefix, limit=request_limit
+                    )
+                ]
+                if request_limit != limit:
+                    # Object of type islice is not JSON serializable m(
+                    metrics = list(islice(sorted(set(metrics)), limit))
+            elif format == "object":
+                metrics = {
+                    doc["_id"]: doc.data
+                    async for doc in endpoint.docs(
+                        prefix=request_prefix, limit=request_limit
+                    )
+                }
+                if request_limit != limit:
+                    metrics = dict(islice(sorted(metrics.items()), limit))
+
+        return metrics
